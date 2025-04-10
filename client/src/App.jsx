f01b84a6641aaf6fcea73fe5f0f6ad306beec22f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-10 07:46:35 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [showNavBar, setShowNavBar] = useState(true);
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 50) {
+
+      setShowNavBar(false);
+    } else {
+
+      setShowNavBar(true);
+    }
+    setIsTopOfPage(currentScrollY === 0);
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const shouldShowNavBar = !location.pathname.startsWith("/Dashboard");
+  const shouldShowFooter = !location.pathname.startsWith("/Dashboard");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+        {shouldShowNavBar && (
+          <div
+          className={`fixed top-0 w-full z-50 transition-transform duration-300 ${
+            showNavBar ? "translate-y-0" : "-translate-y-full"
+          } ${
+            isTopOfPage
+              ? "bg-transparent transition-colors duration-500 "
+              : "bg-white/10 backdrop-blur-md shadow-md transition-colors duration-500"
+          }`}
+          >
+            <Nav />
+          </div>
+        )}
+      <Routes>
+        <Route path="*" element={<PageNotFound /> } />
+        <Route path="/" element={<HomePage /> } />
+        <Route path="/signin" element={<Login /> } />
+        <Route path="/AboutUs" element={<AboutUs /> } />
+        <Route path="/Services" element={<Services /> } />
+        <Route path="/Products" element={<Products /> } />
+        <Route path="/Trainers" element={<Trainers /> } />
+
+        <Route path="/Dashboard/" element={<PrivateRoute element={<Dashbaord /> } /> } >
+          <Route path="Home" element={<PrivateRoute element={<DashHome />} /> } />
+        </Route>
+       
+      </Routes>
+      {shouldShowFooter && <Footer />}
+    </BrowserRouter>
   )
 }
-
-export default App
