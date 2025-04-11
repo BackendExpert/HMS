52b86b32797864e95dea6d7a33add0f4caae508b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-11 09:00:45 +0530

    Update

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    indexNo: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: "student", enum: ['student', 'warden', 'admin', 'director'] },
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,5 +4,6 @@ const AuthController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', AuthController.signup)
+router.post('/signin', AuthController.signin)
 
 module.exports = router;
