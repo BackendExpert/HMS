01a42645130ed7b8075f6fc05d24ffd04e328947
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-12 07:26:19 +0530

    Update

--- client/src/components/Dashboard/DashFooter.jsx
+++ client/src/components/Dashboard/DashFooter.jsx
@@ -4,7 +4,7 @@ const DashFooter = () => {
     const currentyear = new Date().getFullYear()
   return (
     <div className='pl-4 py-4 border-t border-gray-300 bg-white md:flex justify-between pr-4'>
-        <h1 className=""><span className='font-semibold'>CopyRight &copy;</span> {currentyear}. <span>All Rights Reserved. | Engineered by <a href="" className='text-blue-800 font-semibold'>JehanKandy</a> </span></h1>
+        <h1 className=""><span className='font-semibold'>CopyRight &copy;</span> {currentyear}. <span>All Rights Reserved. | Engineered by <a href="https://blackalphalabs.vercel.app" target='_blank' className='text-blue-800 font-semibold'>BLACK ALPHA LABS</a> </span></h1>
         <p className="">version 1.0</p>
     </div>
   )
---
+++ client/src/pages/Students/WardenStd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WardenStd = () => {
+  return (
+    <div>WardenStd</div>
+  )
+}
+
+export default WardenStd
