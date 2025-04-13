a3e177f6e16501adeeadc390e0720ef63e00bfb1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-13 17:08:52 +0530

    Update AdminDashboard.jsx

--- client/src/pages/Dashboard/AdminDashboard.jsx
+++ client/src/pages/Dashboard/AdminDashboard.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import DashData from './DashData'
+import CommenPart from './CommenPart'
+import Status from './Status'
 
 const AdminDashboard = () => {
     return (
-        <div className='bg-white m-4 rounded-lg shadow-md'>
-            AdminDashboard
+        <div className="">
+            <div className='bg-white p-4 rounded-lg shadow-md'>
+                <h1 className="text-xl font-semibold text-gray-500">Admin Dashboard </h1>
+                <div className="">
+                    <CommenPart />
+                </div>
+                <div className="">
+                    <DashData />
+                </div>
+            </div>
+            <div className="my-4">
+                <Status />
+            </div>
         </div>
     )
 }
