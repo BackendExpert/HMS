facecc2fd6d541f7bbe4e9605c4cfde03e507c31
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-19 16:41:21 +0530

    Update WardenRooms.jsx

--- client/src/pages/Room/WardenRooms.jsx
+++ client/src/pages/Room/WardenRooms.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import secureLocalStorage from 'react-secure-storage'
+import { Users } from 'lucide-react';
+import axios from 'axios';
 
 const WardenRooms = () => {
-  return (
-    <div>WardenRooms</div>
-  )
+    const username = secureLocalStorage.getItem('loginU')
+    const role = secureLocalStorage.getItem('loginR')
+    const token = localStorage.getItem('login');
+
+    const [vardenrooms, setvardenrooms] = useState([])
+
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_APP_API + '/rooms/vardenrooms', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then(res => setvardenrooms(res.data.Result))
+            .catch(err => console.log(err))
+    }, [token]);
+
+    return (
+        <div>WardenRooms</div>
+    )
 }
 
 export default WardenRooms
