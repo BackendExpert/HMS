b414490bb7ec3428242d528a72a6e8e8ce18b1eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-19 20:36:07 +0530

    Update

--- client/src/components/Dashboard/DashNav.jsx
+++ client/src/components/Dashboard/DashNav.jsx
@@ -69,7 +69,7 @@ const DashNav = () => {
 
                 <div className="mt-5 border-t pt-4">
                     <a
-                        href="#"
+                        href="/Dashboard/Profile"
                         className="flex items-center gap-3 text-gray-600 hover:text-blue-600 hover:bg-gray-100 px-4 py-2 rounded-md transition"
                     >
                         <FaUserCog className="text-xl" />
