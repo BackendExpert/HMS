d888375c8f4deff15fbc57b5729ae9efcbbd6660
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-19 08:07:24 +0530

    Update roomController.js

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,4 +1,5 @@
-const Room = require('../models/Room')
+const Room = require('../models/Room');
+const Student = require('../models/Student');
 
 const RoomController = {
     getallrooms: async(req, res) => {
@@ -6,6 +7,52 @@ const RoomController = {
             const allrooms = await Room.find()
 
             return res.json({ Result: allrooms })
+        }
+        catch(err){
+            console.log(err)
+        }
+    },
+
+    roomAllocationStd: async(req, res) => {
+        try{
+            const eligiblestds = await Student.find({ eligible: true })
+
+            if (!eligiblestds || eligiblestds.length === 0) {
+                return res.json({ error: "No eligible students found" });
+
+            }
+            const availableRooms = await Room.find({ status: 'Availabe' });
+
+            const genderGroupedRooms = {
+                Male: availableRooms.filter(room => room.gender === 'Male'),
+                Female: availableRooms.filter(room => room.gender === 'Female')
+            };
+
+            for (const student of eligiblestds) {
+                const studentGender = student.gender;
+    
+                const room = genderGroupedRooms[studentGender].find(r => r.currentOccupants < r.capacity);
+    
+                if (room) {
+
+                    await RoomAllocation.create({
+                        studentId: student._id,
+                        roomId: room._id
+                    });
+    
+
+                    room.currentOccupants += 1;
+    
+                    if (room.currentOccupants >= room.capacity) {
+                        room.status = 'Full';
+                    }
+    
+                    await room.save();
+                }
+            }
+
+            res.json({ Status: "Success" });              
+            
         }
         catch(err){
             console.log(err)
