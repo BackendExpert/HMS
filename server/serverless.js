d4821f6f4c7f3540fe1c3dd0da71d6946aee6948
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-22 07:40:30 +0530

    Update serverless.js

--- server/serverless.js
+++ server/serverless.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/auth', authRoute);
 app.use('/student', studentRoute);
 app.use('/hostel', hostelRoute);
