4c0ebfd8664ff3b5e75077b493b9b6bb4dbbe982
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-12 22:27:55 +0530

    Update studentRoute.js

--- server/routes/studentRoute.js
+++ server/routes/studentRoute.js
@@ -12,4 +12,7 @@ router.get('/allstudents', authMiddleware, accessMiddleware(['admin', 'director'
 router.get('/getstudent/:id', authMiddleware, accessMiddleware(['admin', 'director']), StudentController.getstdbyID)
 router.get('/vardenstd', authMiddleware, accessMiddleware(['admin', 'director', 'warden']), StudentController.getvardenstd)
 
+router.get('/waitinglist', authMiddleware, accessMiddleware(['admin', 'director']), StudentController.getallstdwaiting)
+router.post('/approvstd/:email', authMiddleware. accessMiddleware(['admin', 'director']), StudentController.approveStd)
+
 module.exports = router;
