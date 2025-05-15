ab092691254bedce7b2bca72c74271bc72850326
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-15 07:30:33 +0530

    Update reallocationRoute.js

--- server/routes/reallocationRoute.js
+++ server/routes/reallocationRoute.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/createReallocation', authMiddleware, accessMiddleware(['student', 'admin', 'director']), ReallocationsController.createReAllocation)
 router.get('/allreqeusts', authMiddleware, accessMiddleware(['admin', 'director']), ReallocationsController.getallreqeusts)
 router.get('/getmyreqeusts', authMiddleware. accessMiddleware(['student', 'admin', 'director']), ReallocationsController.getmyreqeusts)
+router.get('/onerequst/:id', authMiddleware, accessMiddleware(['admin', 'director']), ReallocationsController.getReqeustOne)
 
 module.exports = router;
